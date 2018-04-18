6e9ecd4f4c5093d33ef0124d7791ffa437b70968
PhillipClayton <36546218+PhillipClayton@users.noreply.github.com>
2018-04-18 14:45:42 -0600

    Beginning of adding expressions to the given string for Strange Brew translations.

--- sBrew.js
+++ sBrew.js
@@ -1,4 +1,19 @@
 function toSBrew (toTranslate) {
   var finalTranslation = "";
+  var chanceValue = Math.random();
+  var chanceOfEh = .25;
+  var chanceOfYouHoser = .125;
+  var changeOfYouKnob = .125;
+
+  /* Count the number of periods so we can
+  randomly add expressions to the beginnging/end
+  of each sentence, based on probability.
+  */
+  var numberOfSentences = (toTranslate.match(/\./,g) || []).length;
+  for (var i = 1; i <= numberofSentences) {
+    toTranslate.replace(".", ", eh.");
+  }
+  finalTranslation = toTranslate;
+
   return finalTranslation;
 }
