9c80d6e204fac4e5a4d091962b316f5d3564e78d
PhillipClayton <philclayton8@gmail.com>
2018-04-18 21:50:19 -0600

    Added options for sentences that end in ! (might end with ", you hoser" or ", you knob".  Added probability.

--- sBrew.js
+++ sBrew.js
@@ -2,16 +2,34 @@ function toSBrew (toTranslate) {
   var finalTranslation = "";
   var chanceValue = Math.random();
   var chanceOfEh = .25;
-  var chanceOfYouHoser = .125;
-  var changeOfYouKnob = .125;
+  var chanceOfYouHoser = .20;
+  var changeOfYouKnob = .10;
 
-  /* Count the number of periods so we can
-  randomly add expressions to the beginnging/end
-  of each sentence, based on probability.
+  /*
+  -We want to add 'eh' to the end of some sentences.
+  -We also might want to add "Take off!"" to the beginning of sentences
+  that end with !
+  -Perhaps add 'You hoser' or 'You knob' if the sentence ends with !
+  (This assumes the author is addressing someone.)
+  -Need to check for sentences that already end with 'eh'.
   */
 
   toTranslate = toTranslate.replace(/\./g, function(str) {
-    return ", eh" + str;
+    if (chanceValue <= chanceOfEh) {
+      return ", eh" + str;
+    }
   });
+
+  toTranslate = toTranslate.replace(/\!/g, function(str) {
+    if (chanceValue <= chanceOfYouHoser) {
+      if (chanceValue <= chanceOfYouKnob) {
+        return ", you knob" + str;
+      }
+      else {
+        return ", you hoser" + str;
+      }
+    }
+  });
+
   return toTranslate;
 }
